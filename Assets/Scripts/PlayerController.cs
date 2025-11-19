511f4a84cd8269ff1e4deca547186a17adbe4d0b
Trono41 <trono1441@gmail.com>
2025-11-19 13:51:00 -0800

    left/right movement functional

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,26 +5,31 @@ public class PlayerController : MonoBehaviour
 {
     InputAction move;
     InputAction jump;
-    float moveSpeed = 2f;
-    float jumpForce = 5f;
+    Rigidbody2D rb;
+    public float moveSpeed;
+    public float maxMoveSpeed = 10f;
+    public float jumpForce;
+    bool isGrounded = true;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         move = InputSystem.actions.FindAction("Move");
         jump = InputSystem.actions.FindAction("Jump");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 moveValue = move.ReadValue<Vector2>();
+        rb.linearVelocity = moveValue * moveSpeed;
         if (move.IsPressed())
         {
-            // Debug.Log("Move Pressed! moveValue: " + moveValue);
+            Debug.Log("Move Pressed! moveValue: " + moveValue);
         }
 
-        if (jump.IsPressed())
+        if (jump.IsPressed() && isGrounded)
         {
             // Debug.Log("Jump pressed!");
         }
