2e873db05910f741a191cc9ba6734c37950b8722
Trono41 <trono1441@gmail.com>
2025-11-19 14:20:38 -0800

    Dash function partially implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,17 +7,21 @@ public class PlayerController : MonoBehaviour
 {
     InputAction move;
     InputAction jump;
+    InputAction dash;
     Rigidbody2D rb;
     public float moveSpeed;
-    public float maxMoveSpeed = 10f;
     public float jumpForce;
+    public float dashForce;
+    float direction;
     bool isGrounded = true;
+    bool canDash = true;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         move = InputSystem.actions.FindAction("Move");
         jump = InputSystem.actions.FindAction("Jump");
+        dash = InputSystem.actions.FindAction("Dash");
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -24,10 +30,23 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 moveValue = move.ReadValue<Vector2>();
         rb.linearVelocity = moveValue * moveSpeed;
+        direction = moveValue.x;
 
         if (jump.IsPressed() && isGrounded)
         {
             // Debug.Log("Jump pressed!");
+        }
+
+        if (dash.IsPressed())
+        {
+            if (direction != 0)
+            {
+                rb.AddForceX(dashForce * direction, ForceMode2D.Impulse);
+            }
+            else
+            {
+                rb.AddForceX(dashForce, ForceMode2D.Impulse);
+            }
         }
     }
 }
